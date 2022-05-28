5dbfa6b724ad2b6c992bd52f465856ee642153bf
Maksim Androfagin <androfagin@mail.ru>
2022-05-28 12:50:34 +0400

    Create new project Diplom_SearchSystem

---
+++ src/main/java/Main.java
@@ -0,0 +1,33 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BooleanSearchEngine engine = new BooleanSearchEngine(new File("pdfs"));
+
+        try (ServerSocket serverSocket = new ServerSocket(8989);) {
+            while (true) {
+                try (
+                        Socket socket = serverSocket.accept();
+                        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                        PrintWriter out = new PrintWriter(socket.getOutputStream());
+                ) {
+                    String word = in.readLine();
+
+                    Gson gson = new GsonBuilder().create();
+                    List<PageEntry> list = engine.search(word);
+                    out.println(gson.toJson(list));
+                    out.flush();
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Не могу стартовать сервер");
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/PageEntry.java
@@ -0,0 +1,29 @@
+public class PageEntry implements Comparable<PageEntry> {
+    private final String pdfName;
+    private final int page;
+    private final int count;
+
+    public PageEntry(String pdfName, int page, int count) {
+        this.pdfName = pdfName;
+        this.page = page;
+        this.count = count;
+    }
+
+    @Override
+    public int compareTo(PageEntry pageEntry) {
+        return this.getCount() < pageEntry.getCount() ? 1 : -1;
+    }
+
+    @Override
+    public String toString() {
+        return "PageEntry{" +
+                "pdfName='" + pdfName + '\'' +
+                ", page=" + page +
+                ", count=" + count +
+                '}';
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
